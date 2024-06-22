9a417b49869509ffbcfda4e5c870403c7b739050
mav9rick <koil-3@yandex.ru>
2024-06-22 22:45:51 +0500

    Реализован подсчёт веса файлов по типам

--- FileBrowser/FileTypeStrategy.cpp
+++ FileBrowser/FileTypeStrategy.cpp
@@ -1,3 +1,28 @@
 #include "FileTypeStrategy.h"
 
-FileTypeStrategy::FileTypeStrategy() {}
+// Функция для подсчета размера файлов по их типам (расширениям) в директории и её поддиректориях
+void getDirectorySizeByType(const QString &dirPath, QMap<QString, quint64> &sizeMap)
+{
+    QDir dir(dirPath);
+    QVector<QFileInfo> list = dir.entryInfoList(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot | QDir::Hidden | QDir::System).toVector();
+    for (const QFileInfo &fileInfo : list)
+    {
+        if (fileInfo.isDir())
+        {
+            getDirectorySizeByType(fileInfo.absoluteFilePath(), sizeMap);
+        }
+        else
+        {
+            QString extension = fileInfo.suffix().toLower();
+            sizeMap[extension] += fileInfo.size();
+        }
+    }
+}
+QVector<QVector<QString>> FileTypeStrategy::Calculate(QString dir)
+{
+
+
+
+
+}
+
--- FileBrowser/FileTypeStrategy.h
+++ FileBrowser/FileTypeStrategy.h
@@ -1,10 +1,15 @@
 #ifndef FILETYPESTRATEGY_H
 #define FILETYPESTRATEGY_H
 #include <Strategy.h>
+#include <QDir>
+#include <QMap>
+#include <QFileInfo>
 class FileTypeStrategy : public Strategy
 {
 public:
-    FileTypeStrategy();
+    //using Dirs = QVector<QString>;
+    QVector<QVector<QString>> Calculate(QString dir);
+    void getDirectorySizeByType(const QString &dirPath, QMap<QString, quint64> &sizeMap)
 };
 
 #endif // FILETYPESTRATEGY_H

336ba6681daa02035b5429b83d6d025ee5bbb96f
mav9rick <koil-3@yandex.ru>
2024-06-25 13:22:19 +0500

    Исправление ошибок

--- FileBrowser/main.cpp
+++ FileBrowser/main.cpp
@@ -10,20 +10,23 @@
 #include <iostream>
 using namespace std;
 
-double percent(double fullsize, double size)
+QString percent(int finalSize, int size)
 {
-    if (fullsize != 0 && size != 0)
+    if (finalSize != 0 && size != 0)
     {
-        double percent = 100.0 * size / fullsize;
+        double percent = 100.0 * size / finalSize;
         if (percent < 0.1)
         {
-            return 0.1;
+            return "< 0.1 %";
         }
-        return percent;
+        else
+        {
+            return QString::number(percent, 'f', 2) + " %";
+        }
     }
     else
     {
-        return 0;
+        return "0.00 %";
     }
 }
 
@@ -64,35 +67,21 @@ int main()
             return -1;
         }
         int dirSize = context->calculate(dirPath, map);
-        double finalSize = static_cast<double>(dirSize) / 1024.0;
+        double finalSize = dirSize / 1024.0;
         QString mainDirName = dircheck.fileName();
         out << "/" << mainDirName << " : " << QString::number(finalSize, 'f', 2) << " KB " << "\n";
         out.flush();
         if (finalSize == 0)
         {
-            cout << "Directory is empty.";
-            return -1;
+            cout << "Directory is empty." << "\n";
+            continue;
         }
         for (auto m = map.constBegin(); m != map.constEnd(); ++m)
         {
-            double size = static_cast<double>(m.value()) / 1024.0;
-            double procent = percent(finalSize, size);
+            double size = m.value() / 1024.0;
+            QString procent = percent(finalSize, size);
             QString sizeS = QString::number(size, 'f', 2);
-            QString percentofsize;
-            if (procent == 0)
-            {
-                out << "  |" << "- " << m.key() << ": " << sizeS << " KB " << "Error in calculation (percent = 0)" << "\n";
-            }
-            else if (procent == 0.1)
-            {
-                percentofsize = "< 0.1";
-                out << "  |" << "- " << m.key() << ": " << sizeS << " KB " << percentofsize << "% \n";
-            }
-            else
-            {
-                percentofsize = QString::number(procent, 'f', 2);
-                out << "  |" << "- " << m.key() << ": " << sizeS << " KB " << percentofsize << "% \n";
-            }
+            out << "  |" << "- " << m.key() << ": " << sizeS << " KB " << procent << "\n";
             out.flush();
         }
         command = dirPath;
